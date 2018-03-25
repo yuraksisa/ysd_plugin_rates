a116f41b61083fe5953fc5b4b85f7e1ee82202e4
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2018-03-25 15:00:15 +0200

    show header in price assistant

--- lib/sinatra/ysd_sinatra_rate_price_defs_management.rb
+++ lib/sinatra/ysd_sinatra_rate_price_defs_management.rb
@@ -7,9 +7,10 @@ module Sinatra
         #
         # Price definition page
         #
-        app.get '/admin/rates/price-defs/?*', :allowed_usergroups => ['rates_manager','staff'] do 
+        app.get '/admin/rates/price-defs/?*', :allowed_usergroups => ['rates_manager','staff'] do
 
-          locals = {:rate_price_defs_page_size => 20}
+          locals = {:rate_price_defs_page_size => 20,
+                    :use_factors => SystemConfiguration::Variable.get_value('booking.use_factors_in_rates','false').to_bool}
           load_em_page :rate_price_defs_management, nil, false, :locals => locals
 
         end
